9c844a2a1b2083c6d3c070e2a8749a6b0f964885
Rachael Sewell <rachaelsewell@paradetech.com>
2016-03-31 18:21:52 +0000

    Added default category entries of Spec and Memo

--- install/create_tables-innodb.sql
+++ install/create_tables-innodb.sql
@@ -760,4 +760,5 @@ INSERT INTO tblUsers VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Ad
 INSERT INTO tblUsers VALUES (2, 'guest', NULL, 'Guest User', NULL, '', '', '', 2, 0, '0000-00-00 00:00:00', 0, 0, 0, NULL);
 INSERT INTO tblFolders VALUES (1, 'DMS', 0, '', 'DMS root', UNIX_TIMESTAMP(), 1, 0, 2, 0);
 INSERT INTO tblVersion VALUES (NOW(), 5, 0, 0);
-INSERT INTO tblCategory VALUES (0, '');
+INSERT INTO tblCategory VALUES (0, 'Spec');
+INSERT INTO tblCategory VALUES (1, 'Memo');
